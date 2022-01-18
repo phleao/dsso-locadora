e43d15fcf0b57f2884d66d61f49df5eff0ef2f74
phleao <pedroleao_@hotmail.com>
2022-01-18 18:38:09 -0300

    Adicionando main e controleSistema

--- controle/controlador_sistema.py
+++ controle/controlador_sistema.py
@@ -1,1 +1,17 @@
-# arquivo de controlador do sistema
+#arquivo controlador do sistema
+
+class ControladorSistema:
+
+    def inicializa_sistema(self):
+        self.abre_tela()
+
+    def encerra_sistema(self):
+        exit(0)
+
+    def abre_tela(self):
+        lista_opcoes = {1: "Opcao 1", 2: "Opcao 2", 3: "Opcao 3", 0: "Encerra sistema"}
+
+        while True:
+            opcao_escolhida = self.__tela_sistema.tela_opcoes()
+            funcao_escolhida = lista_opcoes[opcao_escolhida]
+            funcao_escolhida()
---
+++ main.py
@@ -0,0 +1,6 @@
+#arquivo de iniciacao do sistema
+
+from controle.controlador_sistema import ControladorSistema
+
+if __name__ == "__main__":
+  ControladorSistema().inicializa_sistema()
