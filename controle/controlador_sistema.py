93facaa9f53eebec70c9fc8cc78c6098f6b249ff
phleao <pedroleao_@hotmail.com>
2022-01-25 21:54:39 -0300

    adicionando tela de login

--- controle/controlador_sistema.py
+++ controle/controlador_sistema.py
@@ -8,7 +8,17 @@ class ControladorSistema:
         self.__tela_sistema = TelaSistema()
 
     def inicializa_sistema(self):
-        self.abre_tela()
+        if self.abre_tela_login():
+            self.abre_tela()
+
+    def abre_tela(self):
+        lista_opcoes = {1: self.cadastra_livros, 2: self.opcao2, 3: self.opcao3,
+                        0: self.encerra_sistema}
+
+        while True:
+            opcao_escolhida = self.__tela_sistema.tela_opcoes()
+            funcao_escolhida = lista_opcoes[opcao_escolhida]
+            funcao_escolhida()
 
     def cadastra_livros(self):
         self.__controlador_filmes.abre_tela()
@@ -22,11 +32,17 @@ class ControladorSistema:
     def encerra_sistema(self):
         exit(0)
 
-    def abre_tela(self):
-        lista_opcoes = {1: self.cadastra_livros, 2: self.opcao2, 3: self.opcao3,
+    def abre_tela_login(self):
+        lista_opcoes = {1: self.acessa_login, 2: self.opcao2, 3: self.opcao3,
                         0: self.encerra_sistema}
 
         while True:
-            opcao_escolhida = self.__tela_sistema.tela_opcoes()
+            opcao_escolhida = self.__tela_sistema.tela_login()
             funcao_escolhida = lista_opcoes[opcao_escolhida]
             funcao_escolhida()
+            return True
+    
+    def acessa_login(self):
+        print("email: ")
+        print("senha: ")
+        
--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -1,11 +1,22 @@
 #tela inicial do sistema
 
 class TelaSistema:
+
+    def tela_login(self):
+        print("BEM VINDO AO SISTEMA LOCAÇON")
+        print("Escolha sua opcao")
+        print("1 - Login")
+        print("2 - Cadastrar como Funcionário")
+        print("3 - Cadastrar como Cliente")
+        print("0 - Finalizar sistema")
+        opcao = int(input("Escolha a opcao: "))
+        return opcao
+
     def tela_opcoes(self):
         print("-------- LocadaçON ---------")
         print("Escolha sua opcao")
         print("1 - Filmes")
-        print("2 - Opcao 2")
+        print("2 - Locações")
         print("3 - Opcao 3")
         print("0 - Finalizar sistema")
         opcao = int(input("Escolha a opcao: "))
