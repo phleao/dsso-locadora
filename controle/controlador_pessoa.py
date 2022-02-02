8b69d80436ea44b08229192c1b434a8fb8a81f2e
JoaoZaniboni <jooomaior@gmail.com>
2022-02-02 20:46:13 -0300

    adicionando verificaçao se email ja esta sendo usado em uma outra conta no cadastro

--- controle/controlador_pessoa.py
+++ controle/controlador_pessoa.py
@@ -18,16 +18,30 @@ class ControladorPessoa():
     def clientes(self):
         return self.__clientes
 
+    def verificar_se_email_existe(self, email):
+        for funcionario in self.__funcionarios:
+            if funcionario.email == email:
+                return False
+        for cliente in self.__clientes:
+            if cliente.email == email:
+                return False
+        return True
 
     def incluir_funcionario(self):
         dados_funcionario = self.__tela_pessoa.pega_dados_funcionario()
-        funcionarios = Funcionario(dados_funcionario["nome"], dados_funcionario['cpf'], dados_funcionario['email'], dados_funcionario['senha'])
-        self.__funcionarios.append(funcionarios)
+        if self.verificar_se_email_existe(dados_funcionario["email"]) == True:
+            funcionarios = Funcionario(dados_funcionario["nome"], dados_funcionario['cpf'], dados_funcionario['email'], dados_funcionario['senha'])
+            self.__funcionarios.append(funcionarios)
+        else:
+            self.__tela_pessoa.mostra_mensagem("Esse email já está sendo usado")
     
     def incluir_cliente(self):
         dados_cliente = self.__tela_pessoa.pega_dados_cliente()
-        clientes = Cliente(dados_cliente["nome"], dados_cliente['idade'], dados_cliente['email'], dados_cliente['senha'], )
-        self.__clientes.append(clientes)
+        if self.verificar_se_email_existe(dados_cliente["email"]) == True:
+            clientes = Cliente(dados_cliente["nome"], dados_cliente['idade'], dados_cliente['email'], dados_cliente['senha'], )
+            self.__clientes.append(clientes)
+        else:
+            self.__tela_pessoa.mostra_mensagem("Esse email já está sendo usado")
 
     def lista_clientes(self):
         for cliente in self.__clientes:
